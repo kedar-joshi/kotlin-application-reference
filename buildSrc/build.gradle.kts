afc0912fdde628960dbb76a548b16a3e87ed2feb
Kedar Joshi <kedar-joshi@users.noreply.github.com>
2024-01-27 15:15:38 +0530

    Updated Kotlin to v1.9.22

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,8 +10,8 @@ plugins {
 dependencies {
 
 	// Kotlin
-	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-	implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.10")
+	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+	implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.22")
 
 	// Spring Boot
 	implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.3")
